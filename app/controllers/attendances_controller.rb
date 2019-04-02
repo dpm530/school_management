559b848d6d5840e746b5ffc415cb4b73aee3296d
Daniel Palacios <danielpalacios530@gmail.com>
2019-04-02 18:09:42 -0500

    added attendance functionality

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -3,6 +3,23 @@ class AttendancesController < ApplicationController
    def index
    end
 
+   def new
+      @attendance=Attendance.all
+   end
+
+   def create
+      @course=Course.find(params[:course_id])
+      @students=params[:student_ids]
+      puts "="*50
+      puts @students
+      puts "="*50
+      @students.each do |student|
+         Attendance.create(date: params[:date], student_id: student, course_id: params[:course_id])
+      end
+      return redirect_to new_attendance_path
+
+   end
+
    def show
       @course=Course.find(params[:id])
       @student_course=Student.joins(:student_courses).where(student_courses:{ course_id: @course })
@@ -15,15 +32,9 @@ class AttendancesController < ApplicationController
       end
 
       puts "="*50
-      puts @classroom
+      puts params[:student_ids]
       puts "="*50
-
-
    end
-
-   def create
-   end   
-
 
 
 
